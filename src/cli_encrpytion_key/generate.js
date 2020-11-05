0e5a7c552f96c67c70a113c43ec9b638d5ba69fd
Jonathan Budzenski <jon@budzenski.me>
2020-11-05 11:57:44 -0600

    write to kibana.yml

---
+++ src/cli_encrpytion_key/generate.js
@@ -0,0 +1,56 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { safeDump } from 'js-yaml';
+import { appendFileSync } from 'fs';
+import { isEmpty } from 'lodash';
+import { join } from 'path';
+
+import { getConfigDirectory } from '@kbn/utils';
+
+import { confirm } from '../cli_keystore/utils';
+import { Logger } from '../cli_plugin/lib/logger';
+
+export async function generate(encryptionConfig, command) {
+  const logger = new Logger();
+  const keys = encryptionConfig.generate({ force: command.force });
+  logger.log(safeDump(keys));
+
+  if (command.interactive) {
+    if (isEmpty(keys)) {
+      logger.log('No keys to write.  Use the --force flag to generate new keys');
+      return;
+    }
+
+    const write = confirm('Write to kibana.yml?');
+    if (write) {
+      const kibanaYML = join(getConfigDirectory(), 'kibana.yml');
+      appendFileSync(kibanaYML, safeDump(keys));
+    }
+  }
+}
+
+export function generateCli(program, encryptionConfig) {
+  program
+    .command('generate')
+    .description('Generates encryption keys')
+    .option('-i, --interactive', 'interactive output')
+    .option('-f, --force', 'generate new keys for all settings')
+    .action(generate.bind(null, encryptionConfig));
+}
